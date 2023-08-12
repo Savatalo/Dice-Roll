29794b2749d7f5741ad0ad6e3268b8e125221513
Savatalo <mare.programer25@gmail.com>
2023-08-12 22:51:55 +0200

    Full version

--- assets/app.js
+++ assets/app.js
@@ -10,68 +10,24 @@ const rollHistory = [];
 rollDiceBtn.addEventListener("click", () => {
   let randomValue = Math.random();
   value += increment;
-
+  let result = "";
   if (randomValue < 0.15) {
-    diceValue.innerHTML = "⚀";
-    let rollObject = {
-      roll: value,
-      value: "⚀",
-    };
-    rollHistory.push(`<div>
-      <p>roll:${rollObject.roll}</p>
-      <p>${rollObject.value}</p>
-    </div>`);
+    result = "⚀";
   } else if (randomValue > 0.15 && randomValue < 0.35) {
-    diceValue.innerHTML = "⚁";
-    let rollObject = {
-      roll: value,
-      value: "⚁",
-    };
-    rollHistory.push(`<div>
-      <p>roll:${rollObject.roll}</p>
-      <p>${rollObject.value}</p>
-    </div>`);
+    result = "⚁";
   } else if (randomValue > 0.35 && randomValue < 0.55) {
-    diceValue.innerHTML = "⚂";
-    let rollObject = {
-      roll: value,
-      value: "⚂",
-    };
-    rollHistory.push(`<div>
-      <p>roll:${rollObject.roll}</p>
-      <p>${rollObject.value}</p>
-    </div>`);
+    result = "⚂";
   } else if (randomValue > 0.55 && randomValue < 0.75) {
-    diceValue.innerHTML = "⚃";
-    let rollObject = {
-      roll: value,
-      value: "⚃",
-    };
-    rollHistory.push(`<div>
-    <p>roll:${rollObject.roll}</p>
-    <p>${rollObject.value}</p>
-  </div>`);
+    result = "⚃";
   } else if (randomValue > 0.75 && randomValue < 0.85) {
-    diceValue.innerHTML = "⚄";
-    let rollObject = {
-      roll: value,
-      value: "⚄",
-    };
-    rollHistory.push(`<div>
-      <p>roll:${rollObject.roll}</p>
-      <p>${rollObject.value}</p>
-    </div>`);
+    result = "⚄";
   } else {
-    diceValue.innerHTML = "⚅";
-    let rollObject = {
-      roll: value,
-      value: "⚅",
-    };
-    rollHistory.push(`<div>
-      <p>roll:${rollObject.roll}</p>
-      <p>${rollObject.value}</p>
-    </div>`);
+    result = "⚅";
   }
+  diceValue.innerHTML = result;
+  rollHistory.push(`<div class="li-item">
+      <p>roll ${value}: ${result}</p>
+    </div>`);
   rollList.appendChild(newLi);
   newLi.innerHTML = rollHistory.join("");
   console.log(rollHistory);
